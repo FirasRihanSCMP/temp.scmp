c862bc2b3682c5dfd01084ff1c95feee26918691
FirasRihanSCMP <firas.rihan@scmp-lb.com>
2022-03-02 16:21:57 +0200

    v-sws

--- client/src/CarouselScmp/CarouselScmp.jsx
+++ client/src/CarouselScmp/CarouselScmp.jsx
@@ -10,7 +10,7 @@ export default function CarouselScmp(props) {
         <div className={stylesCarousel.bodyWidth}>
             <Carousel fade={true} >
                {props.img ? props.img.map(img => { 
-                  return  <Carousel.Item interval={3000} key={img.id} className={stylesCarousel.carouselmain}>
+                  return  <Carousel.Item interval={img.duration || 3000} key={img.id} className={stylesCarousel.carouselmain}>
                 {img.link?<Link to={`/${img.link}`}><img
                           style={{height: `${props.style}vw` }}
                         className={`d-block w-100 ${stylesCarousel.img}`}
