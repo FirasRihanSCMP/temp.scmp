18b0d90b6896973584719f054434e4ddd3e77994
FirasRihanSCMP <firas.rihan@scmp-lb.com>
2022-03-17 10:33:00 +0200

    v-sws

--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -13,7 +13,7 @@ export default function Events(props) {
   const [Loaded, setLoaded] = useState(false);
  /*  const [startDate, setStartDate]=useState(new Date()); */
   useEffect(() => {
-  Axios.get("https://64.227.166.42/api/Events").then((response)=>{
+  Axios.get("https://188.166.60.231:3002/api/Events").then((response)=>{
     setLoaded(true)
     setEventsList(response.data);
   
